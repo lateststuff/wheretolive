0d9da2ddf9322fe57c0461d20734bd4684e2915f
lateststuff <lateststuff@proton.me>
2025-05-14 09:50:46 -0400

    Fix: Add ChatbotProvider to fix blank page issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
+import { ChatbotProvider } from './context/ChatbotContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <ChatbotProvider>
+        <App />
+      </ChatbotProvider>
     </AuthProvider>
   </React.StrictMode>,
 ) 
